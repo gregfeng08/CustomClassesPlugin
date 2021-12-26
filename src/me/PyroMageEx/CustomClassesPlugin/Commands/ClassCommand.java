2ad05147e8548f40dee7a10a76539e0dc9a45a37
BuildTools <unconfigured@null.spigotmc.org>
2021-12-25 23:37:17 -0800

    added an item manager

--- src/me/PyroMageEx/CustomClassesPlugin/Commands/ClassCommand.java
+++ src/me/PyroMageEx/CustomClassesPlugin/Commands/ClassCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import java.util.*;
 import me.PyroMageEx.CustomClassesPlugin.Main;
+import me.PyroMageEx.Items.ItemManager;
 import net.md_5.bungee.api.ChatColor;
 
 public class ClassCommand implements CommandExecutor{
@@ -36,7 +37,7 @@ public class ClassCommand implements CommandExecutor{
 		if(p.hasPermission("class.use")) {
 			switch(playerClass) {
 			case "zombie":
-				p.getInventory().addItem(this.plugin.zombieHat());
+				p.getInventory().addItem(ItemManager.zombieHat());
 				p.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 100000, 10));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100000, 1));
@@ -46,8 +47,8 @@ public class ClassCommand implements CommandExecutor{
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "skeleton":
-				p.getInventory().addItem(this.plugin.skeleHat());
-				p.getInventory().addItem(this.plugin.skeleBow());
+				p.getInventory().addItem(ItemManager.skeleHat());
+				p.getInventory().addItem(ItemManager.skeleBow());
 				p.getInventory().addItem(new ItemStack(Material.ARROW, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 2));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100000, 1));
@@ -56,8 +57,8 @@ public class ClassCommand implements CommandExecutor{
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "enderman":
-				p.getInventory().addItem(this.plugin.endermanHat());
-				p.getInventory().addItem(this.plugin.enderPearl());
+				p.getInventory().addItem(ItemManager.endermanHat());
+				p.getInventory().addItem(ItemManager.enderPearl());
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 2));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 100000, 1));
@@ -65,8 +66,8 @@ public class ClassCommand implements CommandExecutor{
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "blaze":
-				p.getInventory().addItem(this.plugin.blazeHat());
-				p.getInventory().addItem(this.plugin.blazeBow());
+				p.getInventory().addItem(ItemManager.blazeHat());
+				p.getInventory().addItem(ItemManager.blazeBow());
 				p.getInventory().addItem(new ItemStack(Material.ARROW, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 100000, 3));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 100000, 5));
@@ -75,16 +76,16 @@ public class ClassCommand implements CommandExecutor{
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "wither_skeleton":
-				p.getInventory().addItem(this.plugin.witherSkeletonChest());
-				p.getInventory().addItem(this.plugin.witherSkeletonHat());
-				p.getInventory().addItem(this.plugin.witherSkeletonSword());
+				p.getInventory().addItem(ItemManager.witherSkeletonChest());
+				p.getInventory().addItem(ItemManager.witherSkeletonHat());
+				p.getInventory().addItem(ItemManager.witherSkeletonSword());
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 2));
 				p.getInventory().setItem(8, new ItemStack(Material.COMPASS, 1));
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "silverfish":
-				p.getInventory().addItem(this.plugin.silverfishHat());
-				p.getInventory().addItem(this.plugin.silverfishPick());
+				p.getInventory().addItem(ItemManager.silverfishHat());
+				p.getInventory().addItem(ItemManager.silverfishPick());
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 4));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 100000, 2));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 100000, 1));
@@ -92,11 +93,10 @@ public class ClassCommand implements CommandExecutor{
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "dolphin":
-				p.getInventory().addItem(this.plugin.dolphinHat());
-				p.getInventory().addItem(this.plugin.dolphinFish());
+				p.getInventory().addItem(ItemManager.dolphinHat());
+				p.getInventory().addItem(ItemManager.dolphinFish());
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100000, 3));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 100000, 1));
-				p.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 100000, 1));
 				break;
 			case "reset":
 				p.sendMessage("Your class has been reset.");
