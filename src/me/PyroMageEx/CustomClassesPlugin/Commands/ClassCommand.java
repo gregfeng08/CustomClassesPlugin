1ed243d4cf3649445114ae086eb85ad46b0bf5b6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-26 00:00:46 -0800

    speed nerfs and zombie fix

--- src/me/PyroMageEx/CustomClassesPlugin/Commands/ClassCommand.java
+++ src/me/PyroMageEx/CustomClassesPlugin/Commands/ClassCommand.java
@@ -38,7 +38,7 @@ public class ClassCommand implements CommandExecutor{
 			switch(playerClass) {
 			case "zombie":
 				p.getInventory().addItem(ItemManager.zombieHat());
-				p.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 100000, 10));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 100000, 5));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 100000, 1));
@@ -50,7 +50,7 @@ public class ClassCommand implements CommandExecutor{
 				p.getInventory().addItem(ItemManager.skeleHat());
 				p.getInventory().addItem(ItemManager.skeleBow());
 				p.getInventory().addItem(new ItemStack(Material.ARROW, 1));
-				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 2));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 100000, 1));
 				p.getInventory().setItem(8, new ItemStack(Material.COMPASS, 1));
@@ -59,7 +59,7 @@ public class ClassCommand implements CommandExecutor{
 			case "enderman":
 				p.getInventory().addItem(ItemManager.endermanHat());
 				p.getInventory().addItem(ItemManager.enderPearl());
-				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 2));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 100000, 1));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 100000, 1));
 				p.getInventory().setItem(8, new ItemStack(Material.COMPASS, 1));
@@ -79,14 +79,14 @@ public class ClassCommand implements CommandExecutor{
 				p.getInventory().addItem(ItemManager.witherSkeletonChest());
 				p.getInventory().addItem(ItemManager.witherSkeletonHat());
 				p.getInventory().addItem(ItemManager.witherSkeletonSword());
-				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 2));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 1));
 				p.getInventory().setItem(8, new ItemStack(Material.COMPASS, 1));
 				Bukkit.broadcastMessage(ChatColor.UNDERLINE+""+ChatColor.BOLD+p.getName()+" has selected class: "+playerClass);
 				break;
 			case "silverfish":
 				p.getInventory().addItem(ItemManager.silverfishHat());
 				p.getInventory().addItem(ItemManager.silverfishPick());
-				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 4));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100000, 3));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 100000, 2));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 100000, 1));
 				p.getInventory().setItem(8, new ItemStack(Material.COMPASS, 1));
@@ -97,6 +97,7 @@ public class ClassCommand implements CommandExecutor{
 				p.getInventory().addItem(ItemManager.dolphinFish());
 				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100000, 3));
 				p.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 100000, 1));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 100000, 1));
 				break;
 			case "reset":
 				p.sendMessage("Your class has been reset.");
